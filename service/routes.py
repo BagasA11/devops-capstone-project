b835117b0646eb2794afb2d15eb36f9248cec8ec
BAGAS SEBASTIAN <111202214715@mhs.dinus.ac.id>
2026-05-11 22:08:33 +0700

    test and an endpoint for delete account

--- service/routes.py
+++ service/routes.py
@@ -110,7 +110,15 @@ def update_an_account(id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
-
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    """this account will delete an account of given account_id"""
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if account is None:
+        return abort(status.HTTP_404_NOT_FOUND)
+    account.delete()
+    return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
